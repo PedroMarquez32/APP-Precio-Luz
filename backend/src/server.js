29b7df04ebbb2e278c54f16bcbf5dd6992499b0d
[Kuisyy] <[luis3valderrubio@gmail.com]>
2024-12-12 09:56:50 +0100

    añadiendo cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,16 @@
 /**
  * Config inicial del server
  */
-
+import cors from 'cors';
 import express from 'express';
 import { PORT } from './config/config.js';
 import priceRoutes from './routes/priceRoutes.js';
 
 const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:5173'
+}));
 
 app.use(express.json());
 app.use('/api', priceRoutes);
