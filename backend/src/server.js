28c553e792ab0694297edf7029f0f873717a83e7
DamasoGE <franciscodamasogimenez@gmail.com>
2024-12-12 09:20:19 +0100

    12-12-24-v0

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,20 @@
+import cors from 'cors';
 import express from 'express';
 import path from 'path'
 
-import { PORT, ROOTDIR } from './config/config.js';
+import { PORT } from './config/config.js';
 import userRoutes from './routes/userRoutes.js';
-import rootRoutes from './routes/rootRoutes.js';
 
 const app = express();
 
 
+
+app.use(cors({
+    origin: 'http://localhost:5173'
+}))
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(express.static(path.join(ROOTDIR,"public")));
-
-app.use("/", rootRoutes);
-app.use("/users", userRoutes);
+app.use("/api/user", userRoutes);
 
 app.listen(PORT , ()=>{
     console.log(`Servidor ejecutandose en url: http://localhost:${PORT}`);
