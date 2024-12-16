09588669235fed8369ec5032ec5fbcda6a26a9dc
[Kuisyy] <[luis3valderrubio@gmail.com]>
2024-12-16 13:52:29 +0100

    comprobar renderTime del main

--- frontend/src/helpers/getDataWheater.js
+++ frontend/src/helpers/getDataWheater.js
@@ -3,15 +3,30 @@ const dataWeather = import.meta.env.VITE_GET_WEATHER;
 
 export async function getDataWheater(city) {
     try {
-        const weather = await fetch(`${dataWeather}`);
-        const mapData = new Map();
-        if(!weather.ok){
-            throw new Error("Error: error en la API")
+        // Verificar que se pase un nombre de ciudad válido
+        if (!city || typeof city !== 'string') {
+            throw new Error('El parámetro "city" es obligatorio y debe ser un string.');
         }
-        const data = await weather.json();
-        mapData.set("Wheaters",data);
-        return mapData;
+
+        // Hacer la solicitud al backend
+        const response = await fetch(dataWeather, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ city }),
+        });
+
+        // Verificar si la respuesta es válida
+        if (!response.ok) {
+            throw new Error('Error en la solicitud al backend.');
+        }
+
+        // Parsear los datos obtenidos
+        const data = await response.json();
+
+        return data;
     } catch (error) {
-        console.error("Error: ", error);
+        console.error('Error al obtener los datos del clima:', error.message);
     }
 }
